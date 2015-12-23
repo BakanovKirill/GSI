d6376194a122cff8877c865b444c475f10c1a377
Grigoriy Tsarik <artgrem@gmail.com>
2015-12-24 01:10:45 +0200

    update tests for signals

--- src/gsi/tests.py
+++ src/gsi/tests.py
@@ -13,7 +13,7 @@ class GsiSignalsTests(TestCase):
         self.assertEqual(tile.id, area.tiles.through.objects.get(tile_id=tile.id).id)
 
     def test_remove_empty_area_by_removing_tile(self):
-        '''test signal added_update_area_for_each_tile'''
+        '''test signal remove_empty_area_by_removing_tile'''
 
         tile1, created1 = Tile.objects.get_or_create(name='test1')
         tile2, created2 = Tile.objects.get_or_create(name='test2')
