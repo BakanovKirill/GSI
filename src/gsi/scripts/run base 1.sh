53c4a01f8b0d83a7c149d126a5d8fdf87664b604
Grigoriy Tsarik <artgrem@gmail.com>
2016-01-04 14:27:14 +0200

    add create new scripts

---
+++ src/gsi/scripts/run base 1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+source $/home/w23/mattgsi/bin/250m_config
--- src/gsi/utils.py
+++ src/gsi/utils.py
@@ -1,3 +1,5 @@
+import os
+
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -20,11 +22,27 @@ def validate_status(status):
 
 def make_run(run_base, user):
     from gsi.models import Run, Log, RunStep, OrderedCardItem
+
     run = Run.objects.create(run_base=run_base, user=user)
     log = Log.objects.create(name="run_%s" % run.id)
     run.log = log
     run.save()
     first_card = OrderedCardItem.objects.filter(sequence__runbase=run_base).first()
     step = RunStep.objects.create(parent_run=run, card_item=first_card)
+
+    print 'run_base ============ ', run_base
+    print 'path ============ ', os.getcwd()
+
     #TODO: make scripts for each step
+    OUTPUT_DIR = os.getcwd() + '/src/gsi/scripts/'
+    first_line = '#!/bin/bash'
+    GSI_HOME = '/home/w23/mattgsi'
+    RESOLUTION_ENV_SCRIPT = GSI_HOME + '/bin/'
+    resolution = run.run_base.resolution
+
+    f = open(OUTPUT_DIR+str(run.run_base)+'.sh', 'w+')
+    f.writelines(first_line+'\n')
+    f.writelines('source $'+RESOLUTION_ENV_SCRIPT+str(resolution)+'_config'+'\n')
+    f.close()
+
     return {'run': run, 'step': step}
