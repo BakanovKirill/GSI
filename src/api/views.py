730117b92138302a7aa3fc376d13f0159d7c64b3
Grigoriy Tsarik <artgrem@gmail.com>
2016-02-09 13:31:06 +0200

    update link for api runID.seqID.cardID

--- src/api/views.py
+++ src/api/views.py
@@ -4,21 +4,38 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 
 from core.utils import validate_status
-from gsi.models import Run, RunStep
+from gsi.models import Run, RunStep, CardSequence
+from cards.models import CardItem
 
+# update the status of the runs
+# <RUN_ID>.<SEQUENCE_ID>.<CARD_ID>
+# example:
+# http://indy4.epcc.ed.ac.uk:/run/20.5.1/?status=running
 
 @api_view(['GET'])
 def update_run(request, run_id):
-    """ update the status of the runs """
+    """
+    update the status cards of the runs
+
+    """
 
     data = validate_status(request.query_params.get('status', False))
+    value_list = run_id.split('.')
 
     if data['status']:
         try:
-            current_run = Run.objects.get(id=run_id)
+            current_run = Run.objects.get(id=value_list[0])
+            current_sequence = CardSequence.objects.get(id=value_list[1])
+            current_card = CardItem.objects.get(id=value_list[2])
+            run_step_card = RunStep.objects.get(
+                parent_run=current_run,
+                card_item__card_item=current_card)
+            run_step_card.state = data['status']
             current_run.state = data['status']
+            run_step_card.save()
             current_run.save()
         except ObjectDoesNotExist as e:
             data['status'] = False
