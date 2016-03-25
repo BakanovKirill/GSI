37a08cf294ac16592a132977909fae3a03c82b92
Grigoriy Tsarik <artgrem@gmail.com>
2016-03-25 07:32:14 +0200

    add dropdown for main menu and user menu

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -16,7 +16,6 @@ function onOffSubMenuUser() {
     menuElem.classList.toggle('open');
     $(".icon-visible-user").toggle();
     document.getElementById("dropdownUser").classList.toggle("show");
-    //document.getElementById("dropdownMenu").classList.toggle("show");
   };
 }
 
@@ -26,8 +25,10 @@ function showSubMenu() {
 
 function showSubMenuUser() {
   document.getElementById("dropdownUser").classList.toggle("show");
-  //document.getElementById("dropdownUser").classList.toggle("show");
-  //$(".icon-visible-user").toggle();
+}
+
+function showSubMenuStaticData() {
+  document.getElementById("subMenu").classList.toggle("show");
 }
 
 function invisibleDropMenu() {
@@ -37,25 +38,59 @@ function invisibleDropMenu() {
       var i;
       event = event || window.event;
       var target = event.target || event.srcElement;
-      //var fa_class = target.className;
-      //var fa_elem = document.getElementsByClassName(fa_class > i);
-      //var fa_down = document.getElementsByClassName('fa-chevron-down');
-
-      //alert(fa_elem === fa_down);
 
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
 
         if (openDropdown.classList.contains('show') && target.id !== "title") {
           openDropdown.classList.remove('show');
+          //$(".icon-visible").toggle();
+          var menuElem = document.getElementById('subMenu');
+          //menuElem.classList.toggle('hide');
+        }
+      }
+    }
+    if (!event.target.matches('.dropbtn-user')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content-user");
+      var i;
+      event = event || window.event;
+      var target = event.target || event.srcElement;
+
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+          $(".icon-visible-user").toggle();
         }
       }
     }
   }
 }
 
+//function invisibleDropMenuUser() {
+//  window.onclick = function(event) {
+//    if (!event.target.matches('.dropbtn-user')) {
+//      var dropdowns = document.getElementsByClassName("dropdown-content-user");
+//      var i;
+//      event = event || window.event;
+//      var target = event.target || event.srcElement;
+//
+//      for (i = 0; i < dropdowns.length; i++) {
+//        var openDropdown = dropdowns[i];
+//
+//        if (openDropdown.classList.contains('show') || target.id !== "title") {
+//          openDropdown.classList.remove('show');
+//          $(".icon-visible-user").toggle();
+//        }
+//      }
+//    }
+//  }
+//}
+
 $(document).ready(function(){
   onOffSubMenu();
+  //invisibleDropMenuUser();
   invisibleDropMenu();
   onOffSubMenuUser();
 });
