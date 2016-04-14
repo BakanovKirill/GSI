a88461294de470c64d8a8a3975a104b0a053b7fe
Grigoriy Tsarik <artgrem@gmail.com>
2016-04-14 15:52:23 +0300

    update api

--- src/api/views.py
+++ src/api/views.py
@@ -47,9 +47,13 @@ def update_run(request, run_id):
             log_file.writelines('RUNCARDS_{0}:\n'.format(card.id))
             log_file.writelines('STATUS:\n')
             log_file.writelines(str(now) + '\n')
-            log_file.writelines(str(state) + '\n\n')
-            log_file.writelines('RunStep:\n')
-            log_file.writelines('step => {0} :: id => {1}\n'.format(str(step), str(step.id)))
+            log_file.writelines(str(state) + '\n')
+            log_file.writelines('====== RunStep:\n')
+            log_file.writelines('name => {0} :: id => {1}\n'.format(str(step), str(step.id)))
+            log_file.writelines('====== Run:\n')
+            log_file.writelines('name => {0} :: id => {1}\n'.format(str(run), str(run.id)))
+            log_file.writelines('====== OrderedCardItem:\n')
+            log_file.writelines('name => {0} :: id => {1}\n\n\n'.format(str(card), str(card.id)))
             # log_file.close()
 
             # for step in steps:
