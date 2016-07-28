857fb06db5b515b4b570cf50b95fb36c24a1898d
Grigoriy Tsarik <artgrem@gmail.com>
2016-07-28 22:38:23 +0300

    change PROCESS_NUM

--- src/gsi/settings.py
+++ src/gsi/settings.py
@@ -234,7 +234,7 @@ LOGGING = {
 
 EXECUTE_FE_COMMAND = '/home/gsi/gsi_files/bin/execute_FE_command'
 PATH_RUNS_SCRIPTS = '/lustre/w23/mattgsi/scripts/runs'
-PROCESS_NUM = 4
+PROCESS_NUM = 2
 
 # number paginations
 NUM_PAGINATIONS = 7
