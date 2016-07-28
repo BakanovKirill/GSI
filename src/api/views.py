8f091764aea3a2878788032b79116f231d806a4c
Grigoriy Tsarik <artgrem@gmail.com>
2016-07-28 20:28:28 +0300

    fixed bug in api import modules

--- src/api/views.py
+++ src/api/views.py
@@ -47,15 +47,17 @@ def update_run(request, run_id):
     api_run = open(path_file, 'a')
 
     data = validate_status(request.query_params.get('status', False))
+    value_list = run_id.split('.')
 
     api_run.writelines('RUN {0}:\n'.format(run_id))
     # api_run.writelines('RUN ID {0}:\n'.format(run_card_id))
     api_run.writelines('STATUS {0}:\n'.format(data['status']))
+    api_run.writelines('VAL LIST {0}:\n'.format(value_list))
     api_run.writelines('\n\n\n')
     api_run.close
 
 
-    value_list = run_id.split('.')
+
     run_card_id = value_list[0]
     card_sequence_id = value_list[1]
     order_card_item_id = value_list[2]
