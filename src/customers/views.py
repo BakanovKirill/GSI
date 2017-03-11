25631efb2c6758b35e3e07445ad0383d73ee7500
Grigoriy <artgrem@gmail.com>
2017-03-11 02:31:05 +0200

    added simplekml lyb and added test file

--- src/customers/views.py
+++ src/customers/views.py
@@ -999,6 +999,18 @@ def customer_section(request):
     # if remove:
     coord = []
     
+    import simplekml
+    kml = simplekml.Kml()
+    pol = kml.newpolygon(name='A Polygon')
+    pol.outerboundaryis = [(18.333868,-34.038274), (18.370618,-34.034421),
+                           (18.350616,-34.051677),(18.333868,-34.038274)]
+    pol.innerboundaryis = [(18.347171,-34.040177), (18.355741,-34.039730),
+                           (18.350467,-34.048388),(18.347171,-34.040177)]
+    pol.style.linestyle.color = simplekml.Color.green
+    pol.style.linestyle.width = 5
+    pol.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.green)
+    kml.save("/data/work/virtualenvs/gsi/GSI/src/media/kml/Polygon Styling.kml")
+    
     print 'coord 11 ======================== ', coord
     
     # default GEOTIFF coordinates
