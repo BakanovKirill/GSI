21bce81d7b699a975768f2007b48f9862d8a5bb5
Grigoriy <artgrem@gmail.com>
2017-03-11 05:19:27 +0200

    change style kml file

--- src/customers/views.py
+++ src/customers/views.py
@@ -1000,6 +1000,16 @@ def customer_section(request):
     coord = []
     
     import simplekml
+    
+    kml = simplekml.Kml()
+    pol = kml.newpolygon(name='A Polygon')
+    pol.outerboundaryis = [(18.333868,-34.038274), (18.370618,-34.034421),
+                           (18.350616,-34.051677),(18.333868,-34.038274)]
+    pol.innerboundaryis = [(18.347171,-34.040177), (18.355741,-34.039730),
+                           (18.350467,-34.048388),(18.347171,-34.040177)]
+    kml_path = os.path.join(KML_PATH, 'Polygon.kml')
+    kml.save(kml_path)
+
     kml = simplekml.Kml()
     pol = kml.newpolygon(name='A Polygon 2')
     pol.outerboundaryis = [(18.333868,-34.038274), (18.370618,-34.034421),
