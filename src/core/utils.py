d935ac09c4e9ec46df5aaca2d0dc5d1c3dd836e4
Grigoriy <artgrem@gmail.com>
2017-05-16 15:17:55 +0300

    update log in the make_run

--- src/core/utils.py
+++ src/core/utils.py
@@ -556,21 +556,21 @@ def make_run(run_base, user):
 				first_script['run'].state = 'running'
 				first_script['run'].save()
 			else:
-				ex_fe_com = Popen(
+				out, err = Popen(
 				    'nohup {0} {1} {2} &'.format(
 				        EXECUTE_FE_COMMAND,
 				        first_script['run'].id,
 				        first_script['card'].id
 				    ),
-				    shell=True
-				)
+				    shell=True, stdout=PIPE
+				).communicate()
 
 				# print 'out =========================== ', out
 				# print 'err =========================== ', err
 
 				####################### write log file
-				# file_message_error += '\OUT: {0}\n'.format(out)
-				# file_message_error += '\ERR: {0}\n'.format(err)
+				file_message_error += '\OUT: {0}\n'.format(out)
+				file_message_error += '\ERR: {0}\n'.format(err)
 				#######################
 				
 				first_script['step'].state = 'running'
