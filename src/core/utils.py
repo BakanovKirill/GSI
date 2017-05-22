c94d21b5b2fee595790365bb92134adc56400410
Grigoriy <artgrem@gmail.com>
2017-05-22 12:30:44 +0300

    update command for the log file

--- src/core/utils.py
+++ src/core/utils.py
@@ -633,8 +633,9 @@ def make_run(run_base, user):
 				# ).communicate()
 				command = '/home/gsi/LOGS/run_test'
 				comm = 'ssh-copy-id -i ~/.ssh/id_rsa.pub gsi@cirrus.epcc.ed.ac.uk'
+				comm_2 = 'grups'
 				
-				out, err = Popen('{0}'.format(command),
+				out, err = Popen('{0}'.format(comm_2),
 							    shell=True, stdout=PIPE, stderr=PIPE
 							).communicate()
 					
