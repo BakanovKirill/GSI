eb16e2de1878ad815a38ca025bb69e7390cedecb
Grigoriy <artgrem@gmail.com>
2017-08-03 13:27:04 +0300

    update scripts_ajax.js for kml file

--- src/static/js/scripts_ajax.js
+++ src/static/js/scripts_ajax.js
@@ -405,7 +405,7 @@ function setPolygon(obj) {
                     // var text = kmlEvent.featureData.description;
                     // showInContentWindow(text);
                     
-                    alert("EVENT KML: "+kmlEvent.featureData.infoWindowHtml);
+                    alert("EVENT KML: "+kmlEvent.featureData);
                     
                     var centerX = document.documentElement.clientWidth / 2;
                     var centerY = document.documentElement.clientHeight / 2;
