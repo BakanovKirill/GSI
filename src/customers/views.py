b715e8395b397e98214b12559d77c2a6066556fa
Grigoriy <artgrem@gmail.com>
2017-10-31 00:38:29 +0200

    corrected color aoi

--- src/customers/views.py
+++ src/customers/views.py
@@ -2651,13 +2651,13 @@ def customer_section(request):
 
                 # data = os.path.join(absolute_kml_url, polygon)
 
-                if request.get_host() == '127.0.0.1:8000':
-                    data = 'http://indy4.epcc.ed.ac.uk/media/kml/tree-count-1.kml'
-                else:
-                    # data = os.path.join(absolute_kml_url, select_polygon)
-                    data = select_polygon.kml_url
+                # if request.get_host() == '127.0.0.1:8000':
+                #     data = 'http://indy4.epcc.ed.ac.uk/media/kml/tree-count-1.kml'
+                # else:
+                #     # data = os.path.join(absolute_kml_url, select_polygon)
+                #     data = select_polygon.kml_url
 
-                # data = select_polygon.kml_url
+                data = select_polygon.kml_url
 
                 # url_kml = 'https://doc-0s-b8-docs.googleusercontent.com/docs/securesc/t2e26pal3cvqhgci00iokqk6s7mn29k8/npdqs37ivknd5no63g2s59sujo7ea4cq/1508486400000/08805881789186013635/08805881789186013635/0B306OTCpD7KOOGpLRmUxMFo0eHc?e=download&gd=true&access_token=ya29.Gl3qBI_oyJOnMvXyhievRlD-Ir3mcdjWDBxVaZUT0ECADsYKqcqLxlljpQVJt5EOspboz53JzNPu_w5XwsEpc19Cy1p-WZTpPPvzyqz3uT465cmw4pyrhQf6fkBaypk'
                 # url_kml = 'http://indy4.epcc.ed.ac.uk/media/kml/Scotland.kml'
